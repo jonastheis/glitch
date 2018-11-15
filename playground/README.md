bf2a49a856f7ea5ecd96fb531f0bcb2b9b7313e2
kianenigma <Kian.peymani@gmail.com>
2018-11-15 13:14:19 +0100

    adds initial playground build system for glfw

---
+++ playground/README.md
@@ -0,0 +1,31 @@
+# Playground 
+
+This is where we will try things out. 
+
+### Libraries.
+
+- Download a copy of [glfw](http://www.glfw.org/docs/latest/index.html) in the libraries folder. I think compiling is not needed and the top-level makefile will build it automatically.
+- Download a copy of [glad](https://glad.dav1d.de/) in the libraries folder. Download options: 
+  - lang: C/CPP
+  - gl version: 3.3+
+  - specification: openGl
+  - profile: core
+  - I added all the plugins as well
+  
+Finally, libraries folder should be like this: 
+
+```bash
+- libraries
+    - glad 
+        - include
+        - src
+    - glfw
+        - include
+        - src
+        - ... 
+```
+
+# run 
+
+- run `cmake . ` and then `make` in the source folder
+- or open in clion
---
+++ playground/source/main.c
@@ -0,0 +1,30 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        printf("Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        printf("Failed to initialize GLAD\n");
+        return -1;
+    }
+
+    return 0;
+}
