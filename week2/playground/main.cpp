bd3a3b6844d14ba800b8bff0245660a93e2bf15c
kianenigma <Kian.peymani@gmail.com>
2018-11-21 22:46:31 +0100

    adds the absolute minimum needed to run a set of shaders

--- week2/playground/main.cpp
+++ week2/playground/main.cpp
@@ -23,9 +23,86 @@ extern EGLDisplay display; // eglGetDisplay — return an EGL display connection
 extern EGLSurface pBuffer; // eglCreatePbufferSurface — create a new EGL pixel buffer surface
 extern EGLContext ctx;
 
+typedef struct 
+{
+    GLuint       *counterList;
+    int         numCounters;
+    int         maxActiveCounters;
+} CounterInfo;
+
+void getGroupAndCounterList(GLuint **groupsList, int *numGroups, CounterInfo **counterInfo) {
+    GLint          n;
+    GLuint        *groups;
+    CounterInfo   *counters;
+
+    glGetPerfMonitorGroupsAMD(&n, 0, NULL);
+    groups = (GLuint*) malloc(n * sizeof(GLuint));
+    glGetPerfMonitorGroupsAMD(NULL, n, groups);
+    *numGroups = n;
+
+    *groupsList = groups;
+    counters = (CounterInfo*) malloc(sizeof(CounterInfo) * n);
+    for (int i = 0 ; i < n; i++ )
+    {
+        glGetPerfMonitorCountersAMD(groups[i], &counters[i].numCounters,
+         &counters[i].maxActiveCounters, 0, NULL);
+
+        counters[i].counterList = (GLuint*)malloc(counters[i].numCounters * 
+          sizeof(int));
+
+        glGetPerfMonitorCountersAMD(groups[i], NULL, NULL,
+            counters[i].numCounters, 
+            counters[i].counterList);
+    }
+
+    *counterInfo = counters;
+}
+
+int getCounterByName(char *groupName, char *counterName, GLuint *groupID, GLuint *counterID) {
+    int          numGroups;
+    GLuint       *groups;
+    CounterInfo  *counters;
+    int          i = 0;
+
+    if (!countersInitialized) {
+        getGroupAndCounterList(&groups, &numGroups, &counters);
+        countersInitialized = 1;
+    }
+
+    for ( i = 0; i < numGroups; i++ ) {
+       char curGroupName[256];
+       glGetPerfMonitorGroupStringAMD(groups[i], 256, NULL, curGroupName);
+       if (strcmp(groupName, curGroupName) == 0)
+       {
+           *groupID = groups[i];
+           break;
+       }
+   }
+
+   if ( i == numGroups )
+        return -1;           // error - could not find the group name
+
+    for ( int j = 0; j < counters[i].numCounters; j++ )
+    {
+        char curCounterName[256];
+        
+        glGetPerfMonitorCounterStringAMD(groups[i],
+         counters[i].counterList[j], 
+         256, NULL, curCounterName);
+        if (strcmp(counterName, curCounterName) == 0)
+        {
+            *counterID = counters[i].counterList[j];
+            return 0;
+        }
+    }
+
+    return -1;           // error - could not find the counter name
+}
 
 int main( int argc, char** argv ) {
     egl_setup();
+
+
 
     GLuint VAO;
     glGenVertexArrays(1, &VAO);
@@ -33,27 +110,27 @@ int main( int argc, char** argv ) {
 
     GLfloat vertex_buffer_data[] = {
        0.5f,  0.5f, 0.0f,
-    };
+   };
 
     // vertex_buffer id
-    GLuint vertex_buffer;
+   GLuint vertex_buffer;
     // Generate 1 buffer
-    glGenBuffers(1, &vertex_buffer);
+   glGenBuffers(1, &vertex_buffer);
     // The following commands will talk about our 'vertex_buffer' buffer
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+   glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     // Give our vertices to OpenGL.
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+   glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
 
 
     // create shaders 
-    Shader shader(
-            "/data/exec-test/tr.vs",
-            "/data/exec-test/tr.fs");
+   Shader shader(
+    "/data/exec-test/tr.vs",
+    "/data/exec-test/tr.fs");
 
 
     // draw. Could go inside a loop? 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
     shader.use(); // equivalent of glUseProgram
 
     // 1st attribute buffer : vertices
@@ -66,7 +143,7 @@ int main( int argc, char** argv ) {
        GL_FALSE,           // normalized?
        0,                  // stride
        (void*)0            // array buffer offset
-    );
+       );
     glDrawArrays(GL_POINTS, 0, 1); // Starting from vertex 0; 3 vertices total -> 1 triangle
     glDisableVertexAttribArray(0);
 
